91f5ed1d91f1d4b5c7162bf5b21a2db0684acd8c
Kamiel <kamieldevisser@gmail.com>
2022-12-02 11:44:21 +0100

    add 2022 day 1, day 2

---
+++ 2022/Day 1/Day 1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+
+	private static int[] output;
+
+	static void Main(string[] args)
+	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		ReadInput(File.ReadAllLines(@"../../../../input.txt"), out output);
+
+		SolveA();
+		SolveB();
+
+		stopwatch.Stop();
+		System.Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
+	}
+
+	private static void ReadInput(string[] input, out int[] output)
+	{
+		output = input.Select(line => string.IsNullOrEmpty(line) ? -1 : int.Parse(line)).ToArray();
+	}
+
+	private static void SolveA()
+	{
+		int[] calories = output;
+		int currentCalories = 0, maxCalories = 0;
+		for (int i = 0; i < calories.Length; i++)
+		{
+
+			if (calories[i] == -1)
+			{
+				if (currentCalories > maxCalories)
+				{
+					maxCalories = currentCalories;
+				}
+				currentCalories = 0;
+			}
+			else
+			{
+				currentCalories += calories[i];
+			}
+		}
+
+		Console.WriteLine(maxCalories);
+	}
+
+	private static void SolveB()
+	{
+		int[] inCalories = output;
+		int currentCalories = 0, maxCalories = 0;
+		List<int> outCalories = new List<int>();
+		for (int i = 0; i < inCalories.Length; i++)
+		{
+
+			if (inCalories[i] == -1)
+			{
+				outCalories.Add(currentCalories);
+				currentCalories = 0;
+			}
+			else
+			{
+				currentCalories += inCalories[i];
+			}
+		}
+
+		outCalories.Sort();
+		outCalories.Reverse();
+
+		Console.WriteLine(outCalories[0] + outCalories[1] + outCalories[2]);
+	}
+
+}
---
+++ 2022/Day 2/Day 2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+
+	private static string[][] output;
+	private static int score;
+
+	static void Main(string[] args)
+	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		ReadInput(File.ReadAllLines(@"../../../../input.txt"), out output);
+
+		SolveA();
+		SolveB();
+
+		stopwatch.Stop();
+		System.Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
+	}
+
+	private static void ReadInput(string[] input, out string[][] output)
+	{
+		output = input.Select(line => line.Split(' ')).ToArray();
+	}
+
+    private static void PlayGameA(string p1, string p2)
+    {
+        switch (p1)
+        {
+			case "A":
+                switch (p2)
+                {
+					case "X":
+						score += 1 + 3;
+						break;
+					case "Y":
+						score += 2 + 6;
+						break;
+					case "Z":
+						score += 3 + 0;
+						break;
+                }
+				break;
+			case "B":
+				switch (p2)
+				{
+					case "X":
+						score += 1 + 0;
+						break;
+					case "Y":
+						score += 2 + 3;
+						break;
+					case "Z":
+						score += 3 + 6;
+						break;
+				}
+				break;
+			case "C":
+				switch (p2)
+				{
+					case "X":
+						score += 1 + 6;
+						break;
+					case "Y":
+						score += 2 + 0;
+						break;
+					case "Z":
+						score += 3 + 3;
+						break;
+				}
+				break;
+		}
+    }
+
+	private static void PlayGameB(string p1, string p2)
+	{
+		switch (p1)
+		{
+			case "A":
+				switch (p2)
+				{
+					case "X":
+						score += 3 + 0;
+						break;
+					case "Y":
+						score += 1 + 3;
+						break;
+					case "Z":
+						score += 2 + 6;
+						break;
+				}
+				break;
+			case "B":
+				switch (p2)
+				{
+					case "X":
+						score += 1 + 0;
+						break;
+					case "Y":
+						score += 2 + 3;
+						break;
+					case "Z":
+						score += 3 + 6;
+						break;
+				}
+				break;
+			case "C":
+				switch (p2)
+				{
+					case "X":
+						score += 2 + 0;
+						break;
+					case "Y":
+						score += 3 + 3;
+						break;
+					case "Z":
+						score += 1 + 6;
+						break;
+				}
+				break;
+		}
+	}
+
+	private static void SolveA()
+	{
+		score = 0;
+		output.ToList().ForEach(parts => PlayGameA(parts[0], parts[1]));
+		Console.WriteLine("Outcome A: " + score);
+	}
+
+	private static void SolveB()
+	{
+		score = 0;
+		output.ToList().ForEach(parts => PlayGameB(parts[0], parts[1]));
+		Console.WriteLine("Outcome B: " + score);
+	}
+}
