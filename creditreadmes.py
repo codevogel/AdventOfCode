476a1aaa99d0fd4eb5a74bd5f69028e405b71b5c
Kamiel <kamieldevisser@gmail.com>
2022-12-02 11:59:01 +0100

    update readmes credit script

--- creditreadmes.py
+++ creditreadmes.py
@@ -1,15 +1,16 @@
+currentYear = 2022
 currentDay = 12
 
 for day in range(1, currentDay):
-    with (open("./day" + str(day) +"/README.MD") as file):
+    with (open("./" + str(currentYear) + "/day" + str(day) +"/README.MD") as file):
         lines = file.readlines()
-        if not (lines[0].startswith("# Day")):
+        if not (lines[0].startswith("--- Day")):
             continue
         lines.insert(0, "It's contents are originally written by Eric Wastl, and slightly modified with my answers.\n\n\n")
         lines.insert(0, "Advent of Code is a registered trademark in the United States.\n")
-        lines.insert(0, "This readme is archived from https://adventofcode.com/2021/day/" + str(day) + " and may not be commercially redistributed.\n")
+        lines.insert(0, "This readme is archived from https://adventofcode.com/" + str(currentYear) + "/day/" + str(day) + " and may not be commercially redistributed.\n")
 
-    with (open("./day" + str(day) +"/README.MD", "w") as file):
+    with (open("./" + str(currentYear) + "/day" + str(day) +"/README.MD") as file):
         for item in lines:
             file.write(item)
     
