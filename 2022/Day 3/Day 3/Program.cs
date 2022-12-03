f82c2a63ab4c7811b707967c6fcef2eb1d5e40df
Kamiel <kamieldevisser@gmail.com>
2022-12-03 14:08:00 +0100

    add 2022 day 3

--- 2022/Day 3/Day 3/Program.cs
+++ 2022/Day 3/Day 3/Program.cs
@@ -1,36 +1,54 @@
 ﻿using System.Diagnostics;
 
-class Program
+static class Program
 {
 
-	private static string[][] output;
+	private static string[][] outputA;
+	private static string[][] outputB;
 
 	static void Main(string[] args)
 	{
 		Stopwatch stopwatch = new Stopwatch();
 		stopwatch.Start();
+		ReadInput(File.ReadAllLines(@"../../../../input.txt"), out outputA, out outputB);
 
-		ReadInput(File.ReadAllLines(@"../../../../input.txt"), out output);
-
-		SolveA();
-		SolveB();
+		Console.WriteLine("Outcome A: " +
+			outputA.Select(parts => GetCommonChar(parts[0], parts[1])).Select(c => char.IsUpper(c) ? c - 'A' + 27 : c - 'a' + 1).Sum());
+		Console.WriteLine("Outcome B: " +
+			outputB.Select(rucksack => GetCommonChar(rucksack[0], rucksack[1], rucksack[2])).Select(c => char.IsUpper(c) ? c - 'A' + 27 : c - 'a' + 1).Sum());
 
 		stopwatch.Stop();
 		Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
 	}
 
-	private static void ReadInput(string[] input, out string[][] output)
+	private static void ReadInput(string[] input, out string[][] outputA, out string[][] outputB)
 	{
-
+		outputA = input.Select(line => new string[] { line.Substring(0, line.Length / 2), line.Substring(line.Length / 2) }).ToArray();
+		outputB = Chunk(input, 3).Select(chunk => chunk.ToArray()).ToArray();
 	}
 
-	private static void SolveA()
+	/// <summary>
+	/// Break a list of items into chunks of a specific size (from https://stackoverflow.com/a/6362642/12094608 )
+	/// </summary>
+	public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize)
 	{
-		Console.WriteLine("Outcome A: ");
+		while (source.Any())
+		{
+			yield return source.Take(chunksize);
+			source = source.Skip(chunksize);
+		}
 	}
 
-	private static void SolveB()
+	private static char GetCommonChar(string x, string y, string z = "" )
 	{
-		Console.WriteLine("Outcome B: ");
+		for (char c = 'A'; c <= 'z'; c++) // beware: does some unnessecary symbol char checks
+		{
+			if (x.Contains(c) && y.Contains(c))
+				if (string.IsNullOrEmpty(z))
+					return c;
+				else if (z.Contains(c))
+					return c;
+		}
+		return '0';
 	}
 }
