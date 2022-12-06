deec5e56a62ea2bb894950a8e8b7685add17b142
Kamiel <kamieldevisser@gmail.com>
2022-12-06 12:48:36 +0100

    fix indentation

--- 2022/Day 6/Day 6/Program.cs
+++ 2022/Day 6/Day 6/Program.cs
@@ -5,25 +5,25 @@ class Program
 
 	static string messageCopy;
 
-static void Main(string[] args)
-{
-	Stopwatch stopwatch = new Stopwatch();
-	stopwatch.Start();
-
-	string message = File.ReadAllLines(@"../../../../input.txt")[0];
-	Console.WriteLine("Outcome A:" + (message.Length - FilterPacket(message, 4).Length + 4));
-	Console.WriteLine("Outcome B:" + (message.Length - FilterPacket(message, 14).Length + 14));
-
-	stopwatch.Stop();
-	Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
-}
-
-private static string FilterPacket(string message, int length)
-{
-	if (message.Substring(0,length).Distinct().Count() == length)
+	static void Main(string[] args)
 	{
-		return message;
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		string message = File.ReadAllLines(@"../../../../input.txt")[0];
+		Console.WriteLine("Outcome A:" + (message.Length - FilterPacket(message, 4).Length + 4));
+		Console.WriteLine("Outcome B:" + (message.Length - FilterPacket(message, 14).Length + 14));
+
+		stopwatch.Stop();
+		Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
 	}
-	return FilterPacket(string.Concat(message.Skip(1)), length);
-}
+
+	private static string FilterPacket(string message, int length)
+	{
+		if (message.Substring(0,length).Distinct().Count() == length)
+		{
+			return message;
+		}
+		return FilterPacket(string.Concat(message.Skip(1)), length);
+	}
 }
