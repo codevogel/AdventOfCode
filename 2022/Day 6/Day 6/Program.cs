121c3cd7169ef7969e3c9d7f0c0e8a75e57d3063
Kamiel <kamieldevisser@gmail.com>
2022-12-06 12:35:54 +0100

    add day 6

---
+++ 2022/Day 6/Day 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+
+	static string messageCopy;
+
+static void Main(string[] args)
+{
+	Stopwatch stopwatch = new Stopwatch();
+	stopwatch.Start();
+
+	string message = File.ReadAllLines(@"../../../../input.txt")[0];
+	Console.WriteLine("Outcome A:" + (message.Length - FilterPacket(message, 4).Length + 4));
+	Console.WriteLine("Outcome B:" + (message.Length - FilterPacket(message, 14).Length + 14));
+
+	stopwatch.Stop();
+	Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
+}
+
+private static string FilterPacket(string message, int length)
+{
+	if (message.Substring(0,length).Distinct().Count() == length)
+	{
+		return message;
+	}
+	return FilterPacket(string.Concat(message.Skip(1)), length);
+}
+}
