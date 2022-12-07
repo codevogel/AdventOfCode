31eb51e42e4d494f6b0f4dd238b1219760d34923
Kamiel <kamieldevisser@gmail.com>
2022-12-07 23:17:40 +0100

    pretty output

--- 2022/Day 7/Day 7/Program.cs
+++ 2022/Day 7/Day 7/Program.cs
@@ -19,64 +19,64 @@ class Program
 		Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
 	}
 
-    private static void Solve(IEnumerable<string> input)
-    {
+	private static void Solve(IEnumerable<string> input)
+	{
 		List<Directory> directories = new List<Directory>();
 		Stack<Directory> directoryStack = new Stack<Directory>();
 
 		while(input.Count() > 0)
-        {
+		{
 			string command = input.ElementAt(0);
 			if (command.StartsWith(CD))
-            {
+			{
 				string dirName = command.Substring(CD.Length + 1);
 				if (string.Equals(dirName, BACK))
-                {
+				{
 					// Go back one directory
 					directoryStack.Pop();
-                }
+				}
 				else
-                {
+				{
 					// Add new directory
 					Directory newDir = new Directory(dirName);
 					directories.Add(newDir);
 
 					// If stack is not empty
 					if (directoryStack.Count > 0)
-                    {
+					{
 						// Add new directory as sub directory of current directory
 						directoryStack.Peek().SubDirectories[newDir.Name] = newDir;
-                    }
+					}
 					// Push new directory onto the stack
 					directoryStack.Push(newDir);
-                }
+				}
 				// Skip handled line
 				input = input.Skip(1);
-            }
+			}
 			else if (command.StartsWith(LS))
-            {
+			{
 				var lsOutput = input.Skip(1).TakeWhile(line => !line.Contains('$'));
 				var filesInThisDir = lsOutput.Where(line => char.IsDigit(line[0])).Select(line => line.Split(' '));
 				// For all files in directory, add them as a subdirectory and set their file size as their name
 				filesInThisDir.ToList().ForEach(file => directoryStack.Peek().SubDirectories[file[1]] = new Directory(file[0]));
 				// Skip input to next command
 				input = input.Skip(lsOutput.Count() + 1);
-            }
-        }
+			}
+		}
 		// Solve A:
-		Console.WriteLine(directories.Select(directory => directory.FileSize).Where(fileSize => fileSize <= 100000).Sum());
+		Console.WriteLine("A" + directories.Select(directory => directory.FileSize).Where(fileSize => fileSize <= 100000).Sum());
 		// Solve B:
 		int availableSpace = 70000000 - directoryStack.Last().FileSize;
 		int requiredSpace = 30000000;
-		Console.WriteLine(directories.Select(directory => directory.FileSize).Where(fileSize => availableSpace + fileSize >= requiredSpace).Min());
-    }
+		Console.WriteLine("B" + directories.Select(directory => directory.FileSize).Where(fileSize => availableSpace + fileSize >= requiredSpace).Min());
+	}
 
-    public class Directory
-    {
+	public class Directory
+	{
 		public string Name { get; set; }
 		public Dictionary<string, Directory> SubDirectories { get; set; }
 		public int FileSize { get => SubDirectories.Count == 0 ? int.Parse(Name) : SubDirectories.Values.Select(directory => directory.FileSize).Sum(); }
 		public Directory(string name){ Name = name; SubDirectories = new(); }
-    }
+	}
 
 }
