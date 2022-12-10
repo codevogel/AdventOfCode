d7a957ff36e273937718bc825bc15a03595daf38
Kamiel <kamieldevisser@gmail.com>
2022-12-10 17:15:27 +0100

    Update readmes script and readmes

--- creditreadmes.py
+++ creditreadmes.py
@@ -1,16 +1,24 @@
-currentYear = 2022
-currentDay = 12
+import sys;
 
-for day in range(1, currentDay):
-    with (open("./" + str(currentYear) + "/day" + str(day) +"/README.MD") as file):
-        lines = file.readlines()
-        if not (lines[0].startswith("--- Day")):
-            continue
-        lines.insert(0, "It's contents are originally written by Eric Wastl, and slightly modified with my answers.\n\n\n")
-        lines.insert(0, "Advent of Code is a registered trademark in the United States.\n")
-        lines.insert(0, "This readme is archived from https://adventofcode.com/" + str(currentYear) + "/day/" + str(day) + " and may not be commercially redistributed.\n")
+def updateReadmes(year, day):
+    for currentDay in range (1, int(day) + 1):
+        path = "C:\\workspace\\AdventOfCode\\" + year + "\\day" + str(currentDay) + '\\README.MD'
+        with (open(path) as file):
+            lines = file.readlines()
+            if lines[0].startswith("--- Day"):
+                lines.insert(0, "It's contents are originally written by Eric Wastl, and slightly modified with my answers.\n\n\n")
+                lines.insert(0, "Advent of Code is a registered trademark in the United States.\n")
+                lines.insert(0, "This readme is archived from https://adventofcode.com/" + year + "/day/" + str(currentDay) + " and may not be commercially redistributed.\n")
+                for i in range(len(lines)):
+                    puzzleAnsStart = "Your puzzle answer was "
+                    if lines[i].startswith(puzzleAnsStart):
+                        lines[i] = "<details><summary>Click for the correct answer using my puzzle input.</summary>Your puzzle answer was " + lines[i][len(puzzleAnsStart):] + "</details>\n\n"
 
-    with (open("./" + str(currentYear) + "/day" + str(day) +"/README.MD") as file):
-        for item in lines:
-            file.write(item)
+                with (open(path, 'w') as file):
+                    for item in lines:
+                        file.write(item)
     
+if __name__ == "__main__":
+    year = input("Current year?\t")
+    day = input("Current day?\t")
+    updateReadmes(year, day)
