e2aa6d6f06a0d5a2a3dffb863bd10b3add340009
Kamiel <kamieldevisser@gmail.com>
2022-12-10 15:59:33 +0100

    add 10b

--- 2022/Day 10/Day 10/Program.cs
+++ 2022/Day 10/Day 10/Program.cs
@@ -2,87 +2,66 @@
 
 class Program
 {
-
-    static int totalSignalStrength = 0; 
     static void Main(string[] args)
     {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
 
         string[] input = File.ReadAllLines(@"../../../../input.txt");
-        Solve(input);
+        Console.WriteLine("Total signal strength: " + new VideoSystem(program: input).totalSignalStrength);
 
         stopwatch.Stop();
         Console.WriteLine("Found solution in " + stopwatch.ElapsedMilliseconds + "ms");
     }
 
-    private static void Solve(IEnumerable<string> input)
+    class VideoSystem
     {
-        (int xRegister, int cycleCount) data = (1,0);
-        Operation[] operations = input.Select(line => ToOperation(line)).ToArray();
-        foreach (Operation operation in operations)
+        private Sprite sprite;
+        public int totalSignalStrength { get; private set; } = 0; 
+        private int cycleCount = 0, x = 1;
+        public const int LINESIZE = 40;
+        
+        public VideoSystem(IEnumerable<string> program)
         {
-            data = UpdateData(data, operation.Apply(data.xRegister));
-            if (data.cycleCount > 220)
+            sprite = new Sprite();
+            sprite.SetPosition(1);
+            foreach (string line in program)
             {
-                break;
+                if (line.StartsWith('n')) // noOp
+                    Cycle(add: 0);
+                else // AddX
+                {
+                    Cycle(add: 0);
+                    Cycle(add: int.Parse(line.Split(' ')[1]));
+                    sprite.SetPosition(x);
+                }
+                if (cycleCount > 240) { break; }
             }
+            Console.Write('\n');
         }
 
-        Console.WriteLine("Total signal strength: " + totalSignalStrength);
-    }
-
-    private static (int xRegister, int cycleCount) UpdateData((int xRegister, int cycleCount) existingData, (int value, int cycleCount) newData)
-    {
-        int cycleCount = existingData.cycleCount;
-        for (int i = 0; i < newData.cycleCount; i++)
+        public void Cycle(int add)
         {
-            if ((++cycleCount % 20) == 0 && (cycleCount % 40 != 0))
+            // First draw the sprite and determine signal strength
+            sprite.Draw(cycleCount);
+            if (++cycleCount % 20 == 0 && cycleCount % LINESIZE != 0)
+                totalSignalStrength += x * cycleCount;
+            // Then add value at end of the cycle
+            x += add;
+        }
+
+        public class Sprite
+        {
+            private int[] covers = Array.Empty<int>();
+            public void SetPosition(int x) { covers = new int[] { x - 1, x, x + 1 }; }
+            internal void Draw(int crtMarker)
             {
-                int signalStrength = cycleCount * existingData.xRegister;
-                totalSignalStrength += signalStrength;
-                Console.WriteLine("Cyclecount: " + cycleCount + "\tRegister: " + existingData.xRegister + "\tSignal Strength: " + signalStrength);
+                if (crtMarker % 40 == 0 && crtMarker != 0)
+                {
+                    Console.Write('\n');
+                }
+                Console.Write(covers.Contains(crtMarker % VideoSystem.LINESIZE) ? '#' : '.');
             }
-        }
-        return (newData.value, cycleCount);
-    }
-
-    public static Operation ToOperation(string line)
-    {
-        if (line.StartsWith('n'))
-            return new NoOp();
-        else 
-            return new AddX(int.Parse(line.Split(' ')[1]));
-    }
-
-    public abstract class Operation
-    {
-        public int cycleCount = 1;
-
-        public abstract (int value, int cycleCount) Apply(int onValue);
-    }
-
-    public class NoOp : Operation
-    {
-        public override (int value, int cycleCount) Apply(int onValue)
-        {
-            return (onValue, cycleCount);
-        }
-    }
-
-    public class AddX : Operation
-    {
-        public int toAdd;
-
-        public AddX(int toAdd)
-        {
-            this.toAdd = toAdd;
-            this.cycleCount = 2;
-        }
-
-        public override (int value, int cycleCount) Apply(int onValue)
-        {
-            return (onValue + toAdd, cycleCount);
         }
     }
 }
