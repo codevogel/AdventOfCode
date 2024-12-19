b87c8be18ac076642daf23713b31ccf9f9f84429
Kamiel de Visser <kamieldevisser@gmail.com>
2024-12-19 14:39:40 +0100

    add main program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode
+{
+   public class Program
+   {
+
+      public static void Main(string[] args)
+      {
+         if (args.Length != 2)
+         {
+            Console.WriteLine("Usage: dotnet run <year> <day>");
+            return;
+         }
+
+         var year = int.Parse(args[0]);
+         var day = int.Parse(args[1]);
+
+         Console.WriteLine($"Advent of Code {year} - Day {day}");
+
+      }
+   }
+
+}
